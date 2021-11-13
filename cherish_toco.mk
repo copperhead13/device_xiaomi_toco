c119cad441b93be418f4cc50b7af255681856885
Dragos Sabau <dragos.sabau@sincaibm.ro>
2021-11-13 19:30:58 +0200

    toco: CherishOS bringup and Walleye Fingerprint

---
+++ cherish_toco.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from toco device
+$(call inherit-product, device/xiaomi/toco/device.mk)
+
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/cherish/config/common.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := cherish_toco
+PRODUCT_DEVICE := toco
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi Note 10 Lite
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys" \
+    PRODUCT_NAME="toco" \
+    TARGET_DEVICE="toco"
